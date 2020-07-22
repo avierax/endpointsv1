e43941d733ff2a413dd5f5650900245fc68ef339
Ariel Viera <ariel.viera@gmail.com>
2020-07-22 18:36:02 -0400

    initial import

---
+++ src/main/java/systems/appollo/ams/restapi/v1/endpoints/EndpointsApplication.java
@@ -0,0 +1,13 @@
+package systems.appollo.ams.restapi.v1.endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/systems/appollo/ams/restapi/v1/endpoints/EndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package systems.appollo.ams.restapi.v1.endpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
