2b7ddaf0dc55c4fb5dc2d7e2b41b503ebaffca0a
José Sevila <josevilah@gmail.com>
2020-07-24 10:27:19 -0400

    Makes EndpointsApplication.java extends SpringBootServletInitializer

--- src/main/java/systems/appollo/ams/restapi/v1/endpoints/EndpointsApplication.java
+++ src/main/java/systems/appollo/ams/restapi/v1/endpoints/EndpointsApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import systems.appollo.ams.backend.domain.Project;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"systems.appollo.ams.backend.domain", "systems.appollo.ams.backend.refactoring4.vcs.dbdriver"})
-public class EndpointsApplication {
+public class EndpointsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EndpointsApplication.class, args);
